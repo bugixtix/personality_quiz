f60f31b9ab3eab04519a10ee170264013d4ed991
Housam_ <ixtix_bug@hotmail.com>
2023-03-14 00:18:36 +0100

    " #14032023

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import {Link, Route, Routes} from 'react-router-dom'
+import { X0_Start } from './X0_Start/X0_Start';
+import { useState } from 'react';
 function App() {
+  var [name_value_$, setName_value_$] = useState('')
+
+
   return (
     <div className="App">
-      hello
       <Routes>
-        <Route path='/' element={<div>'hellothere'</div>}/>
+        <Route path='/' element={<X0_Start name_value_$={name_value_$} setName_value_$={setName_value_$}/>}/>
       </Routes>
     </div>
   );
