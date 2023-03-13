1fe1e81c350778fea9575b3e58906e48c53555f6
Housam_ <ixtix_bug@hotmail.com>
2023-03-14 00:19:28 +0100

    " #14032023

---
+++ src/X0_Start/X0_Start.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './X0.css'
+export function X0_Start({name_value_$, setName_value_$,}){
+
+    function SETNAME_VALUE(e){setName_value_$(e.target.value)}
+
+    function LINK_CLICK(bool = false){
+        localStorage.setItem('_name_',JSON.stringify(name_value_$))
+        if (bool) window.location.assign(window.location.href + 'X1_Test' )
+    }
+    
+    function ENTER(e){if(e.key === 'Enter' && name_value_$.length > 3 )LINK_CLICK(true)}
+
+    return(
+        <div className="X0_Start">
+            <div className="X0_outer">
+                <h2 className="X0_h2">Gib bitte deinen Namen ein, und klicke auf 'Start' zu starten</h2>
+                <input className="X0_input" type={'text'} placeholder={'Pickachu'} value={name_value_$} onChange={(e)=>SETNAME_VALUE(e)} onKeyDown={(e)=>{ENTER(e)}} ></input>
+                <Link className="X0_Link" to={'/X1_Test'} onClick={LINK_CLICK(false)}> <h2> Start </h2></Link>
+            </div>
+        </div>
+    )
+
+}
