384bf018b6b058c324459795aaab531c21695a11
Housam_ <ixtix_bug@hotmail.com>
2023-03-26 03:05:48 +0200

    " #25032023

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Link, Route, Routes} from 'react-router-dom'
 import { X0_Start } from './X0_Start/X0_Start';
 import { useState } from 'react';
+import { X1_Test } from './X1_Test/X1_Test';
 function App() {
   var [name_value_$, setName_value_$] = useState('')
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={<X0_Start name_value_$={name_value_$} setName_value_$={setName_value_$}/>}/>
+        <Route path='/X1_Test' element={<X1_Test/>} />
       </Routes>
     </div>
   );
--- src/X0_Start/X0_Start.js
+++ src/X0_Start/X0_Start.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import './X0.css'
 export function X0_Start({name_value_$, setName_value_$,}){
-
+    let [clickable_$,setClick_$]=useState(false)
     function SETNAME_VALUE(e){setName_value_$(e.target.value)}
 
     function LINK_CLICK(bool = false){
@@ -16,8 +16,8 @@ export function X0_Start({name_value_$, setName_value_$,}){
         <div className="X0_Start">
             <div className="X0_outer">
                 <h2 className="X0_h2">Gib bitte deinen Namen ein, und klicke auf 'Start' zu starten</h2>
-                <input className="X0_input" type={'text'} placeholder={'Pickachu'} value={name_value_$} onChange={(e)=>SETNAME_VALUE(e)} onKeyDown={(e)=>{ENTER(e)}} ></input>
-                <Link className="X0_Link" to={'/X1_Test'} onClick={LINK_CLICK(false)}> <h2> Start </h2></Link>
+                <input className="X0_input" type={'text'} placeholder={''} value={name_value_$} onChange={(e)=>SETNAME_VALUE(e)} onKeyDown={(e)=>{ENTER(e)}} ></input>
+                <Link className={`X0_Link ${name_value_$.length < 3 && 'unclickable'}`} to={'/X1_Test'} onClick={LINK_CLICK(false)}> <h2> Start </h2></Link>
             </div>
         </div>
     )
